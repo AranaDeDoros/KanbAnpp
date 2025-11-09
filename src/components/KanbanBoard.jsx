c696d126ae5720018f24fa1b16ff4c1261f86913
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-09 00:45:24 -0600

    adding a form to create tasks, for now

--- src/components/KanbanBoard.jsx
+++ src/components/KanbanBoard.jsx
@@ -2,10 +2,20 @@ import React, { useState, useEffect } from "react";
 import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import { useTasks } from "../api/useTasks";
 import { useTokenContext } from "../context/TokenContext";
+import {CreateTaskForm} from "./TaskForm";
 
 export default function KanbanBoard({ projectId }) {
   const token = useTokenContext();
   const { data: tasks, isLoading } = useTasks(projectId, token);
+  const [showCreateForm, setshowCreateForm] = useState(false);
+
+  const handleTaskCreated = (newTask) => {
+    console.log("Task created:", newTask);
+   setColumns({
+     ...columns,
+     backlog: [...columns.backlog, newTask],
+   });
+  };
 
   const [columns, setColumns] = useState({
     backlog: [],
@@ -27,7 +37,7 @@ export default function KanbanBoard({ projectId }) {
   if (!tasks) return <p>No tasks found.</p>;
 
   const handleDragEnd = (result) => {
-    console.log(result)
+    console.log(result);
     const { source, destination } = result;
 
     if (!destination) return; // dropped outside
@@ -55,63 +65,78 @@ export default function KanbanBoard({ projectId }) {
         [destCol]: destTasks,
       });
 
-      // TODO: optionally update backend
       // api.patch(`/tasks/${moved.id}/`, { status: destCol }, { headers: { Authorization: `Bearer ${token}` } })
     }
   };
 
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <div className="grid grid-cols-3 gap-4 p-4">
-        {Object.entries(columns).map(([key, items]) => (
-          <Droppable droppableId={key} key={key}>
-            {(provided) => (
-              <div
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                className="bg-gray-50 p-4 rounded shadow min-h-[400px] h-dvh"
-              >
-                <h3 className="font-bold mb-3 text-gray-700 uppercase text-center">
-                  {key}
-                </h3>
+    <>
+      <button
+        onClick={() => setshowCreateForm(!showCreateForm)}
+        className="px-3 py-1 rounded bg-blue-600 text-white"
+      >
+        add task
+      </button>
+      {showCreateForm ? (
+        <CreateTaskForm
+          token={token}
+          onTaskCreated={handleTaskCreated}
+          projectId={projectId}
+        />
+      ) : null}
 
-                {items.map((task, index) => (
-                  <Draggable
-                    key={task.id}
-                    draggableId={task.id.toString()}
-                    index={index}
-                  >
-                    {(provided) => (
-                      <div
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        ref={provided.innerRef}
-                        className="bg-gray-200 rounded p-2 mb-2  hover:shadow-sm"
-                      >
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <div className="grid grid-cols-3 gap-4 p-4">
+          {Object.entries(columns).map(([key, items]) => (
+            <Droppable droppableId={key} key={key}>
+              {(provided) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  className="bg-gray-50 p-4 rounded shadow min-h-[400px] h-dvh"
+                >
+                  <h3 className="font-bold mb-3 text-gray-700 uppercase text-center">
+                    {key}
+                  </h3>
+
+                  {items.map((task, index) => (
+                    <Draggable
+                      key={task.id}
+                      draggableId={task.id.toString()}
+                      index={index}
+                    >
+                      {(provided) => (
                         <div
-                          className={`${
-                            task.status === "done"
-                              ? "bg-green-500"
-                              : task.status === "wip"
-                              ? "bg-orange-500"
-                              : "bg-blue-500"
-                          }  text-white rounded p-1 mb-1`}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                          className="bg-gray-200 rounded p-2 mb-2  hover:shadow-sm"
                         >
-                          <strong>{task.title}</strong>
+                          <div
+                            className={`${
+                              task.status === "done"
+                                ? "bg-green-500"
+                                : task.status === "wip"
+                                ? "bg-orange-500"
+                                : "bg-blue-500"
+                            }  text-white rounded p-1 mb-1`}
+                          >
+                            <strong>{task.title}</strong>
+                          </div>
+                          <div className="bg-gray-500 text-white rounded p-1 text-sm">
+                            <i>{task.description} </i>
+                          </div>
                         </div>
-                        <div className="bg-gray-500 text-white rounded p-1 text-sm">
-                          <i>{task.description} </i>
-                        </div>
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        ))}
-      </div>
-    </DragDropContext>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          ))}
+        </div>
+      </DragDropContext>
+    </>
   );
 }
